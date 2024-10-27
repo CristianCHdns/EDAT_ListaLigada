fe0ea021150d05e8484ff0470987db171c61b039
CristianCHxs <22030456@itesa.edu.mx>
2024-10-27 17:30:05 -0600

    ool

--- ListaLigada.Logica/SingleList.cs
+++ ListaLigada.Logica/SingleList.cs
@@ -1,6 +1,27 @@
 ﻿namespace ListaLigada.Logica
 {
-    internal class SingleList
+    internal class SingleList <T>
     {
+        private SingleNode<T>? _first;
+
+        public SingleList()
+        {
+            _first = null;
+        }
+
+        public bool isEmpty() => _first == null;
+
+        public void Add(T item) {
+
+            var node = new SingleNode<T>(item);
+            if (isEmpty())
+            {
+                _first = null;
+            }
+            else
+            {
+                var pointer = _first;
+            }
+        }
     }
 }
